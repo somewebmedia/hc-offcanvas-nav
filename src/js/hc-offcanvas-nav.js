4bcb645a0f0211eb32154ee83641ace3b3bcddae
Zeljko Galetic <zeljkogaletic@gmail.com>
2018-09-27 08:40:45 +0800

    3.1.3

--- src/js/hc-offcanvas-nav.js
+++ src/js/hc-offcanvas-nav.js
@@ -1,7 +1,7 @@
 /*!
  * HC Off-canvas Nav
  * ===================
- * Version: 3.1.2
+ * Version: 3.1.3
  * Author: Some Web Media
  * Author URL: http://somewebmedia.com
  * Plugin URL: https://github.com/somewebmedia/hc-offcanvas-nav
