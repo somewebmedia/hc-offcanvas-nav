78510568d736ebbe98709145e1645045b62ff673
Zeljko Galetic <zeljkogaletic@gmail.com>
2020-06-03 19:21:13 +0200

    4.2.2

--- dist/hc-offcanvas-nav.js
+++ dist/hc-offcanvas-nav.js
@@ -1,7 +1,7 @@
 /*
  * HC Off-canvas Nav
  * ===================
- * Version: 4.2.1
+ * Version: 4.2.2
  * Author: Some Web Media
  * Author URL: https://github.com/somewebmedia/
  * Plugin URL: https://github.com/somewebmedia/hc-offcanvas-nav
