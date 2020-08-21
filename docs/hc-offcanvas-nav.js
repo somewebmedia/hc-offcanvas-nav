5f71078316a8d51966ef3600bd72a7801e01509b
Some Web Media <somewebmedia@gmail.com>
2020-08-21 18:06:59 +0200

    5.0.10

--- docs/hc-offcanvas-nav.js
+++ docs/hc-offcanvas-nav.js
@@ -1,7 +1,7 @@
 /*
  * HC Off-canvas Nav
  * ===================
- * Version: 5.0.9
+ * Version: 5.0.10
  * Author: Some Web Media
  * Author URL: https://github.com/somewebmedia/
  * Plugin URL: https://github.com/somewebmedia/hc-offcanvas-nav
