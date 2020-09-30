ea8e4f87aae88de178f5162e1b5ab1447a0794a1
Some Web Media <somewebmedia@gmail.com>
2020-09-30 09:36:26 +0200

    Object.assign polyfill

--- src/js/hc-offcanvas-nav.helpers.js
+++ src/js/hc-offcanvas-nav.helpers.js
@@ -4,6 +4,34 @@
   const hcOffcanvasNav = window.hcOffcanvasNav;
   const document = window.document;
 
+  if (typeof Object.assign !== 'function') {
+    Object.defineProperty(Object, 'assign', {
+      value: function assign(target, varArgs) {
+        'use strict';
+        if (target == null) {
+          throw new TypeError('Cannot convert undefined or null to object');
+        }
+
+        var to = Object(target);
+
+        for (var index = 1; index < arguments.length; index++) {
+          var nextSource = arguments[index];
+
+          if (nextSource != null) {
+            for (var nextKey in nextSource) {
+              if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                to[nextKey] = nextSource[nextKey];
+              }
+            }
+          }
+        }
+        return to;
+      },
+      writable: true,
+      configurable: true
+    });
+  }
+
   let supportsPassive = false;
   try {
     const opts = Object.defineProperty({}, 'passive', {
